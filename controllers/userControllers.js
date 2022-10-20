fa051aa08dedd72a3dabb03ece5f9c7426d1194f
SetyoRian <setyo.wahyu.trianto@gmail.com>
2022-10-20 23:35:25 +0700

    add login and editUser Controller add user authorization add jwt add authentication add new routes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -44,6 +44,86 @@ class UserController{
             res.status(500).json(err);
         })
     }
+
+    static loginUser(req, res) {
+        const { email, password } = req.body;
+        User.findOne({
+            where: {
+                email
+            }
+        })
+        .then(user => {
+            if(!user){
+                throw {
+                    name: "User Login Error",
+                    message: `User with Email ${email} not found`
+                }
+            }
+            const isCorrect = comparePassword(password, user.password);
+            if(!isCorrect) {
+                throw {
+                    name: "User Login Error",
+                    message: `User's password with email ${email} does not match`
+                }
+            }
+            const payload = {
+                id: user.id,
+                email: user.email,
+                username: user.username
+            };
+
+            const token = generateToken(payload);
+            return res.status(200).json({token});
+        })
+        .catch(err => {
+            return res.status(500).json(err);
+        })
+    }
+
+    static editUser(req,res) {
+        const userId = +req.params.userId;
+        const {
+            email,
+            full_name,
+            username,
+            profile_image_url,
+            age,
+            phone_number
+        } = req.body;
+
+        const updateUser = {
+            email,
+            full_name,
+            username,
+            profile_image_url,
+            age,
+            phone_number
+        };
+
+        User.update(
+            updateUser,
+            {
+                where: {
+                    id: userId
+                },
+                returning: true
+            }
+        ).then(result => {
+            let response = {
+                user: {
+                    email: result[1][0].email,
+                    full_name: result[1][0].full_name,
+                    username: result[1][0].username,
+                    profile_image_url: result[1][0].profile_image_url,
+                    age: result[1][0].age,
+                    phone_number: result[1][0].phone_number
+                }
+            }
+            res.status(200).json(response);
+        }).catch(err => {
+            res.status(500).json(err);
+        })
+    }
 }
 
 module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 const UserController = require('../controllers/userControllers');
+const authentication = require('../middlewares/authentication');
+const userAuthorize = require('../middlewares/userAuthorize');
 
 router.post('/users/register', UserController.registerUser);
+router.post('/users/login', UserController.loginUser);
+
+router.use(authentication);
+
+router.use('/users/:userId', userAuthorize);
+router.put('/users/:userId', UserController.editUser);
 
 module.exports = router
